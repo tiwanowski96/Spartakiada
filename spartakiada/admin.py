b91cbbb8994b3863b20a3c8313769549a6f584cb
Tomasz <tomasz.iwanowski@besidethepark.com>
2019-11-07 14:11:13 +0100

    Add admin console view

--- spartakiada/admin.py
+++ spartakiada/admin.py
@@ -1,8 +1,56 @@
 from django.contrib import admin
+from spartakiada.models import Cup, Duel, Game, Member, Partaker, Participation, Player, Team, Tournament
 
-from spartakiada.models import Player, Tournament
 
-admin.site.register(Player)
+class TournamentInline(admin.TabularInline):
+    model = Tournament
+    extra = 1
 
-admin.site.register(Tournament)
+class MemberInline(admin.TabularInline):
+    model = Member
+    extra = 1
+
+class ParticipationInline(admin.TabularInline):
+    model = Participation
+    extra = 1
+
+class PartakerInline(admin.TabularInline):
+    model = Partaker
+    extra = 1
+
+class DuelInline(admin.TabularInline):
+    model = Duel
+    extra = 1
+
+
+
+class CupAdmin(admin.ModelAdmin):
+    inlines = [TournamentInline]
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [TournamentInline]
+
+class PartakerAdmin(admin.ModelAdmin):
+    inlines = [ParticipationInline]
+
+class PlayerAdmin(admin.ModelAdmin):
+    inlines = [MemberInline, PartakerInline]
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [MemberInline, PartakerInline]
+
+class TournamnetAdmin(admin.ModelAdmin):
+    inlines = [ParticipationInline, DuelInline]
+
+admin.site.register(Cup, CupAdmin)
+admin.site.register(Duel)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Member)
+admin.site.register(Partaker, PartakerAdmin)
+admin.site.register(Participation)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Tournament, TournamnetAdmin)
+
+
 
